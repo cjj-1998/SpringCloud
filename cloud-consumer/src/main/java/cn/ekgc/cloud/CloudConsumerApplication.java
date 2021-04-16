534cf9041b5d5b6f8b2b95b809c086d82820fb52
chenjunjie <c1456510119@163.com>
2021-04-16 15:04:42 +0800

    修改bug

--- cloud-consumer/src/main/java/cn/ekgc/cloud/CloudConsumerApplication.java
+++ cloud-consumer/src/main/java/cn/ekgc/cloud/CloudConsumerApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableEurekaClient
 @SpringBootApplication
 public class CloudConsumerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CloudConsumerApplication.class, args);
 	}
